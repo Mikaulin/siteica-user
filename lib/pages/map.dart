0740910315520bf233ee5dafe765aaca76104af4
Mikel Garcia <mikausin@gmail.com>
2021-03-30 19:07:20 +0200

    Added map package and test widget for rendering a map.

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class MapPage extends StatelessWidget {
   static Route<dynamic> route() => MaterialPageRoute(
@@ -12,7 +14,31 @@ class MapPage extends StatelessWidget {
         title: Text("*Map test"),
       ),
       body: Center(
-        child: Text("Hello, Map!"),
+        child: FlutterMap(
+          options: MapOptions(
+            center: LatLng(51.5, -0.09),
+            zoom: 13.0,
+          ),
+          layers: [
+            TileLayerOptions(
+                urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                subdomains: ['a', 'b', 'c']
+            ),
+            MarkerLayerOptions(
+              markers: [
+                Marker(
+                  width: 80.0,
+                  height: 80.0,
+                  point: LatLng(51.5, -0.09),
+                  builder: (ctx) =>
+                      Container(
+                        child: FlutterLogo(),
+                      ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
