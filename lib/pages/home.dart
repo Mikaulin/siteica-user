ee772a92d5d968aa1f1b84f6c2c0b85d4aa4ec5d
Mikel Garcia <mikausin@gmail.com>
2021-04-01 18:01:19 +0200

    Added BLE broadcast basic functionality.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,37 @@
+import 'package:beacon_broadcast/beacon_broadcast.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid_enhanced/uuid.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static Route<dynamic> route() => MaterialPageRoute(
     builder: (context) => HomePage(),
   );
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _enabled = false;
+  BeaconBroadcast beaconBroadcast = BeaconBroadcast();
+  String _uuid = Uuid.randomUuid().toString();
+
+  void _startBroadcast() {
+    setState(() {
+      _enabled = !_enabled;
+    });
+
+    if (_enabled) {
+
+      beaconBroadcast
+          .setUUID(_uuid)
+          .setMajorId(1)
+          .setMinorId(100)
+          .start();
+    } else {
+      beaconBroadcast.stop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +40,21 @@ class HomePage extends StatelessWidget {
         title: Text("*Home test"),
       ),
       body: Center(
-        child: Text("Hello, Home!"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_uuid),
+            Text(
+              _enabled ? 'Emitiendo' : 'Desconectado',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _startBroadcast,
+        tooltip: 'Broadcast',
+        child: Icon(_enabled ? Icons.bluetooth_disabled : Icons.settings_bluetooth),
       ),
     );
   }
